efe3206d8d2da32fa2250fedc1f001f7c98cfff8
diianrhmaa <diianrhma06@gmail.com>
2023-06-24 10:16:12 +0700

    update file

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
+
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,5 +22,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 // const analytics = getAnalytics(app);
-
-// note 
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ import Iconhapus from '@/asset/Iconhapus';
 import {useRouter} from "next/router";
 import {useState, useEffect} from "react";
 import { db } from '@/config/firebase';
-import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
+import { collection, getDocs, doc, deleteDoc, query, orderBy } from 'firebase/firestore';
 
 export default function Home() {
   // buat state untuk menyimpan data buku
   const [buku, setBuku] = useState([]);
+  const [search, setSearch] = useState("");
   const router = useRouter();
   const addBookHandler = () =>{
     router.push("/tambah-buku")
@@ -20,12 +21,14 @@ export default function Home() {
   // buat variabel untuk menyimpan data collection
   const bukuCollectionRef = collection(db, "buku");
 
+  // buat const sort data
+  const sortData = query(bukuCollectionRef, orderBy("tahun_terbit", "desc"));
+  
   // get buku list
   const getBukuList = async() => {
     try {
-      const data = await getDocs(bukuCollectionRef)
-      const filteredData = data.docs.map
-      ((doc) =>({
+      const data = await getDocs(sortData)
+      const filteredData = data.docs.map((doc) =>({
         ...doc.data(),
         id: doc.id,
       }));
@@ -47,7 +50,7 @@ export default function Home() {
     await deleteDoc(bukuDoc);
     getBukuList();
   }
-
+  
   return (
     <Layout>
       <div className="flex justify-center mx-3">
@@ -57,7 +60,14 @@ export default function Home() {
             <h3 className="text-3xl font-semibold">Data Buku Perpustakaan</h3>
           </div>
             <div>
+              {/* button tambah */}
               <button onClick={addBookHandler} className="bg-sky-500 text-white px-6 py-2 rounded-full hover:bg-sky-700 mb-3">Tambah Buku</button>
+              
+              {/* button search */}
+              <div className="flex items-center justify-end">
+                <input type="text" className="w-42 mt-2 mb-4 block rounded-xl border px-3 py-2" onChange={(e) => setSearch(e.target.value)} placeholder="author/book keywords"></input>
+
+              </div>
             </div>
             <div>
               <table className="bg-sky-50 py-10 rounded-xl table-auto">
@@ -71,27 +81,38 @@ export default function Home() {
                   </tr>
                 </thead>
                 <tbody>
-                  {/* function map */}
-                  {buku.map((data) => (
-                  <tr className="hover:bg-sky-200" key={data.id}>
-                    <td className="px-6 py-3">{data.nama_buku}</td>
-                    <td className="px-6 py-3">{data.pengarang}</td>
-                    <td className="px-6 py-3">{data.deskripsi_buku}</td>
-                    <td className="px-6 py-3">{data.tahun_terbit}</td>
-                    <td className="px-6 py-3 flex">
-                      <span onClick={()=>{
-                        updateBookHandler(data.id);
-                      }}
-                      className="cursor-pointer h-8 w-8 mr-2 hover:text-sky-500"><Iconubah/></span>
-                      <span className="cursor-pointer h-8 w-8 mr-2 hover:text-red-500" onClick={() =>
-                      {
-                        deleteBuku
-                        (data.id);
-                      }}>
-                      <Iconhapus/></span>
-                    </td>
+                  {/*  function map */}
+                  {buku.length >= 1 ? (
+                    buku
+                      .filter((data) =>
+                      data.nama_buku?.toLowerCase().includes(search) || data.pengarang?.toLowerCase().includes(search)
+                  )                  
+                  .map((data) => (
+                    <tr className="hover:bg-sky-200" key={data.id}>
+                      <td scope="col" className="px-6 py-3">{data.nama_buku}</td>
+                      <td scope="col" className="px-6 py-3">{data.pengarang}</td>
+                      <td scope="col" className="px-6 py-3">{data.deskripsi_buku}</td>
+                      <td scope="col" className="px-6 py-3">{data.tahun_terbit}</td>
+                      <td scope="col" className="px-6 py-3 flex">
+                        <span onClick={()=>{
+                          updateBookHandler(data.id);
+                        }}
+                          className="cursor-pointer h-8 w-8 mr-2 hover:text-sky-500"><Iconubah/></span>
+                        <span className="cursor-pointer h-8 w-8 mr-2 hover:text-red-500" onClick={() =>
+                        {
+                          deleteBuku(data.id);
+                        }}
+                        >
+                          <Iconhapus/>
+                        </span>
+                      </td>
                   </tr>
-                  ))}
+                  ))
+              ) : (
+                <tr>
+                  <td className="py-5 text-center" colSpan={6}>Buku ada data buku!</td>
+                </tr>
+              )}
                 </tbody>
               </table>
             </div>
